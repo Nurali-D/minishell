838af63121f0e3a10cb79547bee7ff012dbe4aa5
Lesia Tulune <ltulune@student.21-school.ru>
2021-07-03 19:03:08 +0300

    syntax checker

--- srcs/parser/syntax_error.c
+++ srcs/parser/syntax_error.c
@@ -1,51 +1,65 @@
 #include "minishell.h"
 
-void	check_semicolon(t_msh *ms, int *i)
+int	check_single_quotes(char *line, int i)
 {
-	int	j;
-
-	j = *i;
-	while (j != 0 && ms->line[j])
+	while (line && line[++i])
 	{
-		--j;
-		if (ms->line[j] != ';' && ms->line[j] != ' ')
-			return ;
-		if (ms->line[j] == ';')
-		{
-			if (ms->line[j + 1] == ';' || ms->line[j - 1] == ';')
-				error_function(ms, "syntax error near unexpected token `;;'");
-			else
-				error_function(ms, "syntax error near unexpected token `;'");
-			*i = ft_strlen(ms->line);
-		}
+		if (line[i] == '\'')
+			return 0;
 	}
-	if (j == 0 && (ms->line[j] == ';' || ms->line[j] == ' '))
-	{
-		if (ms->line[j + 1] == ';')
-			error_function(ms, "syntax error near unexpected token `;;'");
-		else
-			error_function(ms, "syntax error near unexpected token `;'");
-		*i = ft_strlen(ms->line);
-	}
+	printf("Not interpret unclosed quotes!\n");
+	return 1;
 }
 
-// void	check_pipe(t_msh *ms, int *i)
-// {
+int	check_double_quotes(char *line, int i)
+{
+	while (line && line[++i])
+	{
+		if (line[i] == '\'' && i > 0 && line[i - 1] != '\\')
+			return 0;
+	}
+	printf("Not interpret unclosed quotes!\n");
+	return 1;
+}
 
-// }
+int	check_semicolon(void)
+{
+	printf("Not interpret semicolon!\n");
+	return (1);
+}
 
-void	check_for_syntax_errors(t_msh *ms)
+int	check_for_syntax_errors(t_msh *ms)
 {
 	int	i;
+	int	j;
 
+	j = 0;
 	i = -1;
 	(void)ms;
 	while (ms->line[++i])
 	{
-		if (ms->line[i] == ';')
-			check_semicolon(ms, &i);
+		while (ms->line && ms->line[++i])
+	{
+		if (ms->line[i] == '\'' && check_single_quotes(ms->line, i) == 1)
+			return (1);
+		else if (ms->line[i] == '\\')
+			i += 1;
+		else if (ms->line[i] == '"' && check_double_quotes(ms->line, &i) == 1)
+			return (1);
+		// else if (ms->line[i] == '$')
+		// 	ms->line = treat_dollar(ms->line, &i, ms->env_list);
+		else if (ms->line[i] == ';')
+			return (check_semicolon());
 		// else if (ms->line[i] == '|')
-		// 	check_pipe(ms, &i);
+		// {
+		// 	check_pipe(ms->line, i);
+		// 	j = i + 1;
+		// }
+		// else if (ms->line[i] == '>' || ms->line[i] == '<')
+		// {
+		// 	check_redirections(ms, &i, j);
+		// 	j = i + 1;
+		// }
 		
 	}
 }
